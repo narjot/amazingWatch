2de231f3c2d380c0fe8b9391ab1a78299e767ac5
narjot <singhnarjot92@gmail.com>
2021-05-10 13:35:14 -0400

    Added http request

---
+++ amazing-watchlist-main/src/app/models/episode.model.ts
@@ -0,0 +1,20 @@
+export class Episode {
+  id: number;
+  name: string;
+  seasonNumber: number;
+  episodeNumber: number;
+  airDate: Date;
+  summary: string;
+  fullNumber: string;
+
+  constructor(episodeInfo) {
+    this.id = episodeInfo.id;
+    this.name = episodeInfo.name;
+    this.seasonNumber = episodeInfo.season;
+    this.episodeNumber = episodeInfo.number;
+    this.airDate = new Date(episodeInfo.airdate);
+    this.summary = episodeInfo.summary;
+    this.fullNumber = `S${(Number(episodeInfo.season) > 9) ? episodeInfo.season : '0' + episodeInfo.season }` +
+                      `E${(Number(episodeInfo.number) > 9) ? episodeInfo.number : '0' + episodeInfo.number }`;
+  }
+}
